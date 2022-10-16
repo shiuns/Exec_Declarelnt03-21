5dafbd4b62a34e3e38b7b6975789e284dbf31d8b
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:10:27 +0800

    加入專案檔案。

---
+++ [作業(Exec_Declarelnt 第三題計算)]-21謝孟勳/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Declarelnt_第三題計算___21謝孟勳
+{
+    //判斷9,202各別是奇數或偶數
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 9;    
+            int number2 = 202;
+            if (number1 % 2 ==0)
+            {
+                Console.WriteLine("是偶數");
+            }
+            else
+            {
+                Console.WriteLine("是奇數");
+            }
+            if (number2 % 2 == 0)
+            {
+                Console.WriteLine("是偶數");
+            }
+            else
+            {
+                Console.WriteLine("是奇數");
+            }
+
+        }
+    }
+}
